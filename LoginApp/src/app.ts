fb65ca15e3648d089d9da7e7d8e03b6fccafa086
Diego Tenorio <diegotenoriodev@gmail.com>
2018-05-18 12:19:21 -0400

    Route added for register and login

--- LoginApp/src/app.ts
+++ LoginApp/src/app.ts
@@ -1,3 +1,22 @@
+import {Router, RouterConfiguration} from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 export class App {
-  message = 'Hello World!';
+  message: string;
+
+  private router: Router;
+
+  configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'Users';
+
+    config.map(
+      [
+        {route: '', moduleId: PLATFORM.moduleName('resources/login'), title: 'Login'},
+        {route: 'register', moduleId: PLATFORM.moduleName('resources/register'), name:'register', title: 'New User'},
+        {route: 'login', moduleId: PLATFORM.moduleName('resources/login'), name: 'login', title: 'Login'},
+      ]
+    );
+
+    this.router = router;
+  }
 }
